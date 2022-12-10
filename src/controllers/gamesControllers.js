481c2dd3fdb5a8d5ea31c06c9b7d7f393321454a
gabrielportelagomes <gabrielgomesportela@gmail.com>
2022-12-10 17:16:57 -0300

    feat: GET request on route /games

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,5 +1,26 @@
 import connection from "../database/db.js";
 
+export async function getGames(req, res) {
+  const { name } = req.query;
+
+  try {
+    if (!name) {
+      const { rows } = await connection.query(
+        `SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id;`
+      );
+      res.send(rows);
+    } else {
+      const { rows } = await connection.query(
+        `SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id WHERE games.name ILIKE $1 || '%';`,
+        [name]
+      );
+      res.send(rows);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
 export async function postGame(req, res) {
   const { name, image, stockTotal, categoryId, pricePerDay } = res.locals.game;
 
--- src/routes/gamesRoutes.js
+++ src/routes/gamesRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { postGame } from "../controllers/gamesControllers.js";
+import { getGames, postGame } from "../controllers/gamesControllers.js";
 import { gameSchemaValidation } from "../middlewares/gameSchemaValidation.js";
 import { gameValidation } from "../middlewares/gameValidation.js";
 
 const gamesRouter = Router();
 
+gamesRouter.get("/games", getGames);
 gamesRouter.post("/games", gameSchemaValidation, gameValidation, postGame);
 
 export default gamesRouter;
