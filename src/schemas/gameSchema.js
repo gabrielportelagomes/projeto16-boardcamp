35f8930027fbc8083c5ae83ba1d08afcba98b2a9
gabrielportelagomes <gabrielgomesportela@gmail.com>
2022-12-13 11:21:32 -0300

    refactor: image schema

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -2,10 +2,7 @@ import joi from "joi";
 
 export const gameSchema = joi.object({
   name: joi.string().required(),
-  image: joi
-    .string()
-    .uri({ scheme: [/https?/] })
-    .required(),
+  image: joi.string().uri().required(),
   stockTotal: joi.number().integer().min(1).required(),
   categoryId: joi.number().integer().required(),
   pricePerDay: joi.number().integer().min(1).required(),
