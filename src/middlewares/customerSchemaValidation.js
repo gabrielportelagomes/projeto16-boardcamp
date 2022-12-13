86853cff303e61d86c95cd0b1312b8bcd49bc690
gabrielportelagomes <gabrielgomesportela@gmail.com>
2022-12-13 15:43:48 -0300

    style: remove console.log

--- src/middlewares/customerSchemaValidation.js
+++ src/middlewares/customerSchemaValidation.js
@@ -2,11 +2,13 @@ import { customerSchema } from "../schemas/customerSchema.js";
 
 export async function customerSchemaValidation(req, res, next) {
   const { name, phone, cpf, birthday } = req.body;
+  console.log(req.body);
 
   const { error } = customerSchema.validate(req.body, { abortEarly: false });
 
   if (error) {
     const erros = error.details.map((detail) => detail.message);
+    console.log(erros)
     return res.status(400).send(erros);
   }
 
